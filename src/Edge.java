c3f41e520da23bac78295f3dadcb4abf4f25d4c5
iless <ilessmirass@gmail.com>
2026-05-14 21:44:40 +0500

    edge

--- src/Edge.java
+++ src/Edge.java
@@ -1,2 +1,23 @@
 public class Edge {
+
+    private Vertex from;
+    private Vertex to;
+
+    public Edge(Vertex from, Vertex to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public Vertex getFrom() {
+        return from;
+    }
+
+    public Vertex getTo() {
+        return to;
+    }
+
+    @Override
+    public String toString() {
+        return from.getValue() + " -> " + to.getValue();
+    }
 }
